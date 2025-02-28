9b4fff3d8d3a18764ac1f30bed35a4f6806fa630
idrissa183 <144667889+idrissa183@users.noreply.github.com>
2025-02-28 16:53:17 +0000

    Ajout d'une ligne de commentaire en JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Ceci est un commentaire mono-ligne en JavaScript
 document.getElementById('welcomeButton').addEventListener('click', function () {
     document.getElementById('welcomeMessage').textContent =
         "Nous sommes ravis de vous accueillir sur le site dédié à l'Ecole Polytechnique de Ouagadougou. Explorez nos programmes et nos initiatives!";
