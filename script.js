d44e83005f08d078447a1b3cb599c1e42f402b48
idrissa183 <ouedraogoidrissa7108@gmail.com>
2025-02-28 17:25:21 +0000

    Updated HTML and JavaScript for new features

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ document.getElementById('welcomeButton').addEventListener('click', function () {
     document.getElementById('welcomeMessage').textContent =
         "Nous sommes ravis de vous accueillir sur le site dédié à l'Ecole Polytechnique de Ouagadougou. Explorez nos programmes et nos initiatives!";
 });
+
+// Ajoutez cette fonction en bas de votre script.js
+function changeDynamicContent() {
+    document.getElementById('dynamicContent').textContent =
+        'Le contenu a été changé grâce à JavaScript!';
+}
+
+// Appelons cette fonction au chargement de la page
+window.onload = function () {
+    changeDynamicContent();
+};
