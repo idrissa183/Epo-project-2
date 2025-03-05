e8ec84a9664904dec086b7c8ad4af4ede6e965ad
idrissa183 <ouedraogoidrissa7108@gmail.com>
2025-03-05 10:46:12 +0000

    Ajout d'une galerie d'images pour l'EPO

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ document.getElementById('welcomeButton').addEventListener('click', function () {
     document.getElementById('welcomeMessage').textContent =
         "Nous sommes ravis de vous accueillir sur le site dédié à l'Ecole Polytechnique de Ouagadougou. Explorez nos programmes et nos initiatives!";
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const images = document.querySelectorAll('#galerie img');
+    images.forEach((image) => {
+        image.addEventListener('click', () => {
+            alert('Vous avez cliqué sur une image !');
+        });
+    });
+});
